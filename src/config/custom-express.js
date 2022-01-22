18b9f84ad07019808adf34c955fb1e6fbbf49904
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-01-22 10:39:25 -0300

    feat: adiciona conexao com banco

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://mongo:mongopass@cluster0.es145.mongodb.net/encurtador_url?retryWrites=true&w=majority")
 
 const routes = require('../app/routes');
 routes(app);
